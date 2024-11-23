525c78895ff99fa14d06b411202a7bab465a1d17
muzzboy <swaggeromo3@gmail.com>
2024-11-22 20:32:54 -0800

    Add files via upload

---
+++ glory in the church 2/Sermon.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("OurLink");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
---
+++ glory in the church 2/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("Link1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
